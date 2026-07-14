725a49461f1270c62944269eae3f61780d2c6fec
Sivanthi <skkanagasababathy@aziro.com>
2026-07-14 18:18:34 +0530

    Create pulsetest.js

---
+++ pulsetest.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Welcome() {
+  return </div>
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>Welcome to your first React component.</p>
+    </div>
+  );
+}
+
+export default Welcome;
